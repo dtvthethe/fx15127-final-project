5d96cc5f9f0cf4c610b9757686319ec639e136bf
dtvthethe <dtvthe@gmail.com>
2023-06-11 15:42:50 +0700

    sm update

--- truffle/contracts/Main.sol
+++ truffle/contracts/Main.sol
@@ -1,24 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "./Session.sol";
-import "./modal/IParticipant.sol";
 
 contract Main is IMain {
+
+    // Structure to hold details of Bidder
+    struct IParticipant {
+        // TODO
+        address account;
+        string fullName;
+        string email;
+        uint256 numberOfSession;
+        uint8 deviation;
+    }
+
     address public admin;
 
     // TODO: Variables
-    uint8 private numberOfUser;
-    address[] public sessions;
-    mapping(address => IParticipant) public participants;
-    mapping(uint256 => address) private mapParticipants;
-    uint8 private numberOfParticipant;
-    uint8 private numberOfSession;
+    uint8 private maxOfUser;
+    mapping(address => IParticipant) private mapParticipants;
+    address[] private participants;
+    address[] private sessions;
 
-    constructor(uint8 _numberOfUser) {
+    constructor(uint8 _maxOfUser) {
         admin = msg.sender;
-        numberOfParticipant = 0;
-        numberOfSession = 0;
-        numberOfUser = _numberOfUser;
+        maxOfUser = _maxOfUser;
     }
 
     // Add a Session Contract address into Main Contract. Use to link Session with Main
@@ -26,39 +32,43 @@ contract Main is IMain {
         // TODO
         Session newSession = Session(session);
         sessions.push(address(newSession));
-        numberOfSession++;
     }
 
     // TODO: Functions
     // Add participant.
     function addParticipant(address _account) public onlyAdmin onlyInNumberOfRangeParticipant {
-        participants[_account] = IParticipant(_account, "", "", 0, 0);
-        mapParticipants[numberOfParticipant] = _account;
-        numberOfParticipant++;
+        mapParticipants[_account] = IParticipant(_account, "", "", 0, 0);
+        participants.push(_account);
         emit AddParticipant(_account, "Add participant success!");
     }
 
     // Update participant.
     function updateParticipant(address _account, string memory _fullName, string memory _email) public {
-        participants[_account].fullName = _fullName;
-        participants[_account].email = _email;
+        mapParticipants[_account].fullName = _fullName;
+        mapParticipants[_account].email = _email;
         emit UpdateParticipant(_account, _fullName, _email);
     }
 
     // Get participant by address.
     function getParticipant(address _account) public view returns (IParticipant memory) {
-        return participants[_account];
+        return mapParticipants[_account];
     }
 
     // Get all participants.
-    function getAllParticipants() public view returns (IParticipant[] memory) {
-        IParticipant[] memory _participants = new IParticipant[](numberOfParticipant);
+    function getAllParticipants() public onlyAdmin view returns (IParticipant[] memory) {
+        IParticipant[] memory _participants = new IParticipant[](participants.length);
 
-        for (uint8 i = 0; i < numberOfParticipant; i++) {
-            _participants[i] = participants[mapParticipants[i]];
+        for (uint8 i = 0; i < participants.length; i++) {
+            _participants.push(mapParticipants[participants[i]]);
         }
 
         return _participants;
+    }
+
+    // Create new Session.
+    function createSession(string _productName, string _description, string[] _images) public onlyAdmin(){
+        Session newSession = new Session(address(this), _productName, _description, _images);
+        emit CreateSession(_productName, _description);
     }
 
     // Modify only admin.
@@ -69,7 +79,7 @@ contract Main is IMain {
 
     // Modify number of participant in ragne.
     modifier onlyInNumberOfRangeParticipant() {
-        require(numberOfParticipant < numberOfUser, "Only create 10 users!");
+        require(participants.length < maxOfUser, "Only create 10 users!");
         _;
     }
 
@@ -78,4 +88,6 @@ contract Main is IMain {
 
     // Event add participant
     event AddParticipant(address _account, string msg);
+
+    event CreateSession(_productName, _description);
 }
--- truffle/contracts/Session.sol
+++ truffle/contracts/Session.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "./modal/ISession.sol";
-import "./lib/UtilityLibrary.sol";
+// import "./lib/UtilityLibrary.sol";
 
 // Interface of Main contract to call from Session contract
 interface IMain {
-    function addSession(address session) external;
-    function getParticipant(address _account) external view returns (IParticipant memory);
+    function addSession(address session) internal;
 }
 
 contract Session {
@@ -16,10 +14,18 @@ contract Session {
     IMain MainContract;
 
     // TODO: Variables
-    ISession private session;
-    adsress[] private participantPricings;
+    enum SESSION_STATUS {CREATED, INPROGRESS, CLOSE};
 
-    using UtilityLibrary for uint256;
+    string private productName;
+    string private description;
+    string[] private images;
+    uint256 private suggestPrice;
+    uint256 private finalPrice;
+    SESSION_STATUS private status;
+    mapping(address => uint256) private mapParticipantPricings;
+    address[] private participantPricings;
+
+    // using UtilityLibrary for uint256;
 
     constructor(
         address _mainContract,
@@ -32,7 +38,13 @@ contract Session {
         MainContract = IMain(_mainContract);
 
         // TODO: Init Session contract
-        session = ISession(_productName, _description, _images, [], 0, 0, SESSION_STATUS.CREATED);
+        // session = ISession(_productName, _description, _images, [], 0, 0, SESSION_STATUS.CREATED);
+        productName = _productName;
+        description = _description;
+        images = _images;
+        suggestPrice = 0;
+        finalPrice = 0;
+        status = SESSION_STATUS.CREATED;
 
         // Call Main Contract function to link current contract.
         MainContract.addSession(address(this));
@@ -41,68 +53,37 @@ contract Session {
     //Update session.
     function updateSession(string _productName, string _description, string[] images) public onlyAdmin {
         // TODO
-        session.productName = _productName;
-        session.description = _description;
-        session.images = _images;
+        productName = _productName;
+        description = _description;
+        images = _images;
     }
 
     // TODO: Functions
     // Set status to INPROGRESS.
     function startSession() public onlyAdmin {
-        session.status = SESSION_STATUS.INPROGRESS;
+        status = SESSION_STATUS.INPROGRESS;
     }
 
     // Set status to CLOSE.
     function closeSession() public onlyAdmin {
-        session.status = SESSION_STATUS.CLOSE;
+        status = SESSION_STATUS.CLOSE;
     }
 
     // Participant pricing.
     function pricing(address _account, uint256 _price) public onlyParticipant onlyInProgress {
-        if (session.participantPrices[_account] == 0) {
+        mapParticipantPricings[_account] = _price;
+
+        if (mapParticipantPricings[_account] == 0) {
+            // check this work?
             participantPricings.push(_account);
         }
-
-        session.participantPrices[_account] = _price;
     }
 
-    // Calculate suggest price.
-    function calculateSuggestPrice() public onlyAdmin {
-        mapping(address => uint256) prices = session.participantPrices;
-        uint256 _suggestPrice = 0;
-        uint256 sumOfPriceWithDeviation = 0;
-        uint256 sumOfDeviation = 0;
-
-        for (uint256 i = 0; i < participantPricings.length; i++) {
-            sumOfPriceWithDeviation += prices[participantPricings[i]] * (100 - saiSoNguoiDinhGiaI);
-            sumOfDeviation += saiSoNguoiDinhGiaI;
-        }
-
-        _suggestPrice = sumOfPriceWithDeviation / ((100 * participantPricings.length) - sumOfDeviation);
-        session.suggestPrice = _suggestPrice;
-    }
-
-    // Calculate deviation in session.
-    function calculateDeviationInSession(address _account) public onlyAdmin onlyFinalPriceMustSetValue returns (uint256) {
-        uint256 _deviation = 0;
-        uint256 _subDeviation = (session.finalPrice - session.participantPrices[_account]).abs();
-        _deviation = (_subDeviation / session.finalPrice) * 100;
-
-        return _deviation;
-    }
-
-    // Calculate deviation.
-    function calculateDeviation(address _account) public onlyAdmin onlyFinalPriceMustSetValue returns (uint256) {
-        uint256 _deviation = 0;
-        // TODO: confirm lai cong thuc tinh deviation.
-        uint256 _subDeviation = (MainContract.getParticipant(_account).deviation * n) + calculateDeviationInSession(_account);
-        _deviation = _subDeviation / (n + 1);
-        return _deviation;
-    }
+    // Caluculate functions.
 
     // Modify only status is INPROGRESS.
     modifier onlyInProgress {
-        require(session.status == SESSION_STATUS.INPROGRESS, "Session is not in progress");
+        require(status == SESSION_STATUS.INPROGRESS, "Session is not in progress");
         _;
     }
 
@@ -121,7 +102,7 @@ contract Session {
 
     // Modify finalPrice must set value.
     modifier onlyFinalPriceMustSetValue() {
-        require(session.finalPrice > 0, "Final price must set value!");
+        require(finalPrice > 0, "Final price must set value!");
         _;
     }
 }
