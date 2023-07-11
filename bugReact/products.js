1360e1130b6bb59cb9abaa2a7d2f4d5fbfdd8d6f
Đàm TVăn Thế <dtvthe@gmail.com>
2023-07-11 07:21:44 +0700

    Add files via upload

---
+++ bugReact/products.js
@@ -0,0 +1,347 @@
+import { app, h } from 'hyperapp';
+import './products.css';
+import JSAlert from 'js-alert';
+import Toastify from 'toastify-js';
+import { config } from '../config';
+
+const Fragment = (props, children) => children;
+
+const Product = ({ product, newProduct, input, create, isAdmin, fn, currentProductIndex }) => {
+  let price = null;
+  return (
+    <div>
+      {product ? (
+        <div class='card product'>
+          <div class='card-header'>
+            <strong>{product.name}</strong>
+          </div>
+          <div class='card-body'>
+            <img
+              class='rounded float-left product-image'
+              src={
+                product.image.startsWith('http')
+                  ? product.image
+                  : '//robohash.org/' + product.image + '?set=set4&bgset=bg2'
+              }
+            ></img>
+
+            <dl class='row'>
+              <dt class='col-sm-4'>Name</dt>
+              <dd class='col-sm-8'>
+                <h5>{product.name}</h5>
+              </dd>
+
+              <dt class='col-sm-4'>Description</dt>
+              <dd class='col-sm-8'>
+                <p>{product.description}</p>
+              </dd>
+
+              <dt class='col-sm-4'>Proposed Price</dt>
+              <dd class='col-sm-8'>
+                {/* <p>$ {product.price / 100}</p> */}
+                <p>$ {product.priceFormat}</p>
+              </dd>
+
+              <dt class='col-sm-4'>Final Price</dt>
+              <dd class='col-sm-8'>
+                <p>$ {product.finalPriceFormat}</p>
+              </dd>
+
+              <dt class='col-sm-4'>Status</dt>
+              <dd class='col-sm-8'>
+                <p>
+                <span class={`badge badge-${config.sessionBadgeClasses[product.status]}`}>
+                  {config.SESSION_STATUS_TEXT[product.status] || 'N/A'}
+                </span>
+                </p>
+              </dd>
+
+              <dt class='col-sm-4'>Your pricing</dt>
+              <dd class='col-sm-8'>
+                <p>{product.myPricingFormat}</p>
+              </dd>
+              {
+                isAdmin &&
+                  <dt class='col-sm-4'>Price range</dt>
+              }
+              {
+                isAdmin &&
+                  <dd class='col-sm-8'>
+                    <p>{product.prices.length > 0 ? product.prices[0] + ' ~ ' + product.prices[1] : '~'}</p>
+                  </dd>
+              }
+            </dl>
+          </div>
+          {isAdmin ? (
+            <div class='card-footer'>
+              <div class='input-group'>
+                <div class='input-group-prepend'>
+                  <button
+                    class='btn btn-outline-primary'
+                    type='button'
+                    onclick={e => fn({ type: 'start', payload: {index: currentProductIndex} })}
+                    disabled={product.status == config.SESSION_STATUS.CREATED ? '' : 'disabled'}
+                  >
+                    Start
+                  </button>
+                </div>
+                <input
+                  type='number'
+                  class='form-control'
+                  placeholder='Final Price'
+                  oninput={e => (price = e.target.value)}
+                  disabled={product.status == config.SESSION_STATUS.PRICING ? '' : 'disabled'}
+                  id="txtPriceAdmin"
+                />
+                <div class='input-group-append'>
+                  <button
+                    class='btn btn-outline-primary'
+                    type='button'
+                    onclick={e => fn({ type: 'stop', payload: {index: currentProductIndex, price}})}
+                    disabled={product.status == config.SESSION_STATUS.PRICING ? '' : 'disabled'}
+                  >
+                    Set price and Stop
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div class='card-footer'>
+              <div class='input-group'>
+                <input
+                  type='number'
+                  class='form-control'
+                  placeholder='Price'
+                  oninput={e => (price = e.target.value)}
+                  disabled={product.status != config.SESSION_STATUS.PRICING}
+                  id="txtPrice"
+                />
+                <div class='input-group-append'>
+                  <button
+                    class='btn btn-primary'
+                    type='button'
+                    disabled={product.status != config.SESSION_STATUS.PRICING}
+                    onclick={e => fn({ type: 'pricing', payload: {index: currentProductIndex, price}})}
+                  >
+                    Propose price
+                  </button>
+                </div>
+              </div>
+              {product.status != config.SESSION_STATUS.PRICING
+                ? (<div class='input-group text-center'>
+                  <p class='w-100 mb-0 mt-1 text-danger'>This input only enable when Session status is <strong>PRICING</strong></p>
+                </div>)
+                : <div></div>}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      {
+        isAdmin && (<div class='card'>
+          <div class='card-header'>
+            <strong>Create new session</strong>
+          </div>
+          <div class='card-body'>
+            <div class='row'>
+              <div class='col-sm-12'>
+                <div class='form-group'>
+                  <label for='name'>Product name</label>
+                  <input
+                    type='text'
+                    class='form-control'
+                    id='name'
+                    value={newProduct.name}
+                    oninput={e => {
+                      input({ field: 'name', value: e.target.value });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='col-sm-12'>
+                <div class='form-group'>
+                  <label for='description'>Product description</label>
+                  <input
+                    type='text'
+                    class='form-control'
+                    id='description'
+                    value={newProduct.description}
+                    oninput={e => {
+                      input({ field: 'description', value: e.target.value });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='col-sm-12'>
+                <div class='form-group'>
+                  <label for='image'>Product image</label>
+                  <input
+                    type='text'
+                    class='form-control'
+                    id='image'
+                    placeholder='http://'
+                    value={newProduct.image}
+                    oninput={e => {
+                      input({ field: 'image', value: e.target.value });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='card-footer'>
+            <button type='submit' class='btn btn-primary' onclick={create}>
+              Create Session
+            </button>
+          </div>
+        </div>)
+      }
+    </div>
+  );
+};
+
+const ProductRow = ({ product, index, select, currentIndex }) => {
+  const handleOnRowClick = (index) => {
+    select(index);
+    const txtPrice = document.getElementById('txtPrice');
+    const txtPriceAdmin = document.getElementById('txtPriceAdmin');
+
+    if (txtPrice != undefined) {
+      txtPrice.value = '';
+    }
+
+    if (txtPriceAdmin != undefined) {
+      txtPriceAdmin.value = '';
+    }
+  };
+
+  return (
+    <tr
+      onclick={e => handleOnRowClick(index)}
+      class={index === currentIndex ? 'active' : ''}
+    >
+      <th scope='row'>{product.no}</th>
+      <td>{product.name}</td>
+      <td>{product.description} </td>
+      <td>$ {product.priceFormat}</td>
+      <td>
+        <span class={`badge badge-${config.sessionBadgeClasses[product.status]}`}>
+          {config.SESSION_STATUS_TEXT[product.status] || 'N/A'}
+        </span>
+      </td>
+    </tr>
+  );
+};
+
+const Products = ({ match }) => (
+  { newProduct, sessions, currentProductIndex, isAdmin },
+  { inputNewProduct, createProduct, selectProduct, sessionFn }
+) => {
+  document.title = `Sessions | ${config.APP_NAME}` || 'N/A';
+
+  const handleSessionFn = async (params) => {
+    const loading = JSAlert.loader('Please wait...');
+
+    try {
+      await sessionFn(params);
+      loading.dismiss();
+      Toastify({
+        text: 'Session status saved!',
+        position: 'center',
+        backgroundColor: config.color.success
+      }).showToast();
+
+      if (params.type == 'pricing' && document.getElementById('txtPrice') != undefined) {
+        document.getElementById('txtPrice').value = '';
+      }
+    } catch (error) {
+      console.log(error);
+      loading.dismiss();
+      Toastify({
+        text: 'Error on handle update session status!',
+        position: 'center',
+        backgroundColor: config.color.error
+      }).showToast();
+
+      if (params.type == 'pricing' && document.getElementById('txtPrice') != undefined) {
+        document.getElementById('txtPrice').value = '';
+      }
+    }
+  }
+
+  const create = async () => {
+    const loading = JSAlert.loader('Please wait...');
+
+    try {
+      await createProduct();
+      loading.dismiss();
+      Toastify({
+        text: 'Session saved!',
+        position: 'center',
+        backgroundColor: config.color.success
+      }).showToast();
+    } catch (error) {
+      console.log(error);
+      loading.dismiss();
+      Toastify({
+        text: 'Error on handle create Session!',
+        position: 'center',
+        backgroundColor: config.color.error
+      }).showToast();
+    }
+  }
+
+  return (
+    <div class='d-flex w-100 h-100'>
+      <div class='bg-white border-right products-list'>
+        <table class='table table-hover table-striped'>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Product</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Proposed price</th>
+              <th scope='col'>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sessions.map((p, i) => {
+              p.no = i + 1;
+              return (
+                <ProductRow
+                  product={p}
+                  index={i}
+                  select={selectProduct}
+                  currentIndex={currentProductIndex}
+                  isAdmin={isAdmin}
+                ></ProductRow>
+              );
+              // return ProductRow(p, i, selectProduct, currentProductIndex);
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div class='pl-2 flex product-detail'>
+        <Product
+          newProduct={newProduct}
+          input={inputNewProduct}
+          create={create}
+          product={sessions[currentProductIndex]}
+          isAdmin={isAdmin}
+          fn={handleSessionFn}
+          currentProductIndex={currentProductIndex}
+        ></Product>
+      </div>
+    </div>
+  );
+};
+
+export { Products };
---
+++ bugReact/sidebar.js
@@ -0,0 +1,170 @@
+import { app, h } from 'hyperapp';
+import { Link } from '@hyperapp/router';
+
+const Fragment = (props, children) => children;
+
+const Profile = ({ profile, register }) => {
+  const hasProfile = profile && profile.nSessions > 0;
+  let newParticipant = {};
+
+  if (profile) {
+    newParticipant = {
+      address: profile.account || '',
+      fullname: profile.fullname || '',
+      email: profile.email || '',
+      isUpdateProfile: true
+    };
+  }
+
+  if (profile) {
+    return hasProfile ? (
+      <div>
+        <li class='nav-title'>{profile.fullname}</li>
+        <li class='nav-item px-3 mb-2'>
+          <div>
+            <small class='text-muted'>
+              <b>Email</b>
+            </small>
+          </div>
+          <small>{profile.email}</small>
+        </li>
+        <li class='nav-item px-3 d-compact-none d-minimized-none mb-2'>
+          <div>
+            <small class='text-muted'>
+              <b>Number of sessions</b>
+            </small>
+          </div>
+          <small>{profile.nSessions} session(s)</small>
+        </li>
+        <li class='nav-item px-3 d-compact-none d-minimized-none mb-2'>
+          <div>
+            <small class='text-muted'>
+              <b>Accuracy</b>
+            </small>
+          </div>
+          <small>{profile.deviation / 100}%</small>
+        </li>
+      </div>
+    ) : (
+      <div>
+        <li class='nav-title'>Register</li>
+        <li class='nav-item px-3 mb-2'>
+          <div>
+            <small class='text-muted'>
+              <b>Fullname</b>
+            </small>
+          </div>
+          <input
+            class='form-control form-control-sm'
+            type='text'
+            value={profile.fullname}
+            oninput={e => {
+              newParticipant.fullname = e.target.value;
+            }}
+          ></input>
+        </li>
+        <li class='nav-item px-3 mb-2'>
+          <div>
+            <small class='text-muted'>
+              <b>Email</b>
+            </small>
+          </div>
+          <input
+            class='form-control form-control-sm'
+            type='email'
+            value={profile.email}
+            oninput={e => {
+              newParticipant.email = e.target.value;
+            }}
+          ></input>
+        </li>
+        <li class='nav-item px-3 mb-2'>
+          <button
+            class='btn  btn-sm btn-ghost-primary btn-block '
+            type='button'
+            onclick={() => register(newParticipant)}
+          >
+            Register
+          </button>
+        </li>
+      </div>
+    );
+  }
+};
+
+const Account = ({ account, balance, isAdmin }) => {
+  return account ? (
+    <div class='sidebar-header'>
+      <img
+        class='img-avatar img-thumbnail'
+        src={'https://robohash.org/' + account}
+      ></img>
+      <div class='balance'>{balance / 1000000000000000000} ETH</div>
+      {isAdmin ? (
+        <h4>
+          <span class='badge badge-pill badge-primary'>Administrator</span>
+        </h4>
+      ) : (
+        <h4>
+          <span class='badge badge-pill badge-secondary'>Member</span>
+        </h4>
+      )}
+      <div class='account'>{account}</div>
+    </div>
+  ) : (
+    <div></div>
+  );
+};
+
+const Sidebar = ({
+  account,
+  balance,
+  isAdmin,
+  profile,
+  register,
+  location
+}) => {
+  const pathname = location.pathname;
+
+  return (
+    <div class='sidebar'>
+      <Account
+        account={account}
+        balance={balance}
+        isAdmin={isAdmin}
+        profile={profile}
+      ></Account>
+      <nav class='sidebar-nav'>
+        <ul class='nav'>
+          <Profile
+            profile={profile}
+            register={register}
+          ></Profile>
+          <li class='nav-divider'></li>
+          <li class='nav-title'>View all</li>
+          <li class='nav-item'>
+            <Link class={`nav-link ${pathname == '/' ? 'active' : ''}`} to='/'>
+              <i class='fa-solid fa-house'></i> Home
+            </Link>
+          </li>
+          <li class='nav-item'>
+            <Link class={`nav-link ${pathname == '/products' ? 'active' : ''}`} to='/products'>
+              <i class='fa-solid fa-scale-balanced'></i> Sessions
+            </Link>
+          </li>
+          <li class='nav-item'>
+            {
+            isAdmin
+              ? (<Link class={`nav-link ${pathname == '/participants' ? 'active' : ''}`} to='/participants'>
+                <i class='fa-solid fa-users'></i> Participants
+              </Link>)
+              : <a class="nav-link cursor-no-drop" style="color: #747474"><i class='fa-solid fa-users'></i> Participants</a>
+            }
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export { Sidebar };
